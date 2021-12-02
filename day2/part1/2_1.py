8701c9b1d7903af7a5a19f5905223ca68957de9e
Andrew Downey <andrew@anooserve.com>
2021-12-02 20:06:04 +0000

    Cleanup and day 2 code.

---
+++ day2/part1/2_1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+import os
+
+
+def main():
+    """ Main entry point of the app """
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, '../input.txt')
+    with open(filename, "r") as myfile:
+        horizontal = 0
+        depth = 0
+        lines = myfile.readlines()
+        for line in lines:
+            command, value = line.split(' ')
+            if(command == 'forward'):
+                horizontal += int(value)
+            elif(command == 'down'):
+                depth += int(value)
+            elif(command == 'up'):
+                depth -= int(value)
+
+        print("Horizontal Distance: " + str(horizontal))
+        print("Final Depth: " + str(depth))
+        print("Multiplied: " + str(horizontal * depth))
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
---
+++ day2/part2/2_2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+import os
+
+
+def main():
+    """ Main entry point of the app """
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, '../input.txt')
+    with open(filename, "r") as myfile:
+        horizontal = 0
+        aim = 0
+        depth = 0
+        lines = myfile.readlines()
+        for line in lines:
+            command, value = line.split(' ')
+            value = int(value)
+            if(command == 'forward'):
+                horizontal += value
+                depth += aim * value
+            elif(command == 'down'):
+                aim += value
+            elif(command == 'up'):
+                aim -= value
+
+        print("Horizontal Distance: " + str(horizontal))
+        print("Final Aim: " + str(aim))
+        print("Final Depth: " + str(depth))
+        print("Multiplied: " + str(horizontal * depth))
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
