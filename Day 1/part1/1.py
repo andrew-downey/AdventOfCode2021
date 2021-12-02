ac78ffd543d0008967f81645a913771cc46fb79f
Andrew Downey <andrew@anooserve.com>
2021-12-02 19:44:11 +0000

    Initial commit

---
+++ Day 1/part1/1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+import os
+
+def main():
+	""" Main entry point of the app """
+	dirname = os.path.dirname(__file__)
+	filename = os.path.join(dirname, '../input.txt')
+	with open(filename, "r") as myfile:
+		lastline = myfile.readline()
+		myline = True
+		increases = 0
+		while myline:
+			myline = myfile.readline()
+			if (myline != "" and int(myline) > int(lastline)):
+					increases+=1
+			lastline = myline
+		print("Increases: " + str(increases))
+
+if __name__ == "__main__":
+	""" This is executed when run from the command line """
+	main()
---
+++ Day 1/part2/2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+import os
+import numpy as np
+
+def main():
+	""" Main entry point of the app """
+	dirname = os.path.dirname(__file__)
+	filename = os.path.join(dirname, '../input.txt')
+	with open(filename, "r") as myfile:
+		lines = myfile.readlines()
+		numbers = [int(num, base=16) for num in lines]
+		moving_averages = np.convolve(numbers, np.ones(3, dtype=int), 'valid')
+		print("Averages: " + str(moving_averages))
+
+		increases = 0
+		lastsum = moving_averages[0]
+		for currentsum in moving_averages:
+			if (currentsum > lastsum):
+				print("Increase")
+				increases+=1
+			else:
+				print("Decrease or Same")
+			lastsum = currentsum
+		print("Increases: " + str(increases))
+
+if __name__ == "__main__":
+	""" This is executed when run from the command line """
+	main()
